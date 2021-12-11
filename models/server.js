e3cf81190d9f35e64f8b4dcb28dad87fcc070a9f
yankie <yakielarapacheco@yahoo.com>
2021-12-11 18:00:09 -0500

    Basic express server as JS class

---
+++ models/server.js
@@ -0,0 +1,34 @@
+
+const express = require('express')
+
+class Server {
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT
+
+        // Middlewares
+        this.middlewares()
+
+        // Routes
+        this.routes()
+    }
+
+    middlewares() {
+        // Public directory
+        this.app.use(express.static('public'))
+    }
+
+    routes() {
+        this.app.get('/api', (req, res) => {
+            res.send('Hello World')
+        })
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server running at port ', process.env.PORT)
+        })
+    }
+}
+
+module.exports = Server
