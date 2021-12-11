82a08bc65c6e24c09d63987a91b4ffafb93b701e
yankie <yakielarapacheco@yahoo.com>
2021-12-11 18:59:44 -0500

    Using PutUsers controller

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 
 const {Router} = require('express')
-const {GetUsers} = require("../controllers/users");
+const {GetUsers,
+    PutUsers} = require("../controllers/users");
 
 const router = Router()
 
 router.get('/', GetUsers)
+
+router.put('/', PutUsers)
 
 module.exports = router
