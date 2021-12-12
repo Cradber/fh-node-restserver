f3b3277fcf9e734a1b7207805851cc0ac01d69d8
yankie <yakielarapacheco@yahoo.com>
2021-12-12 18:12:19 -0500

    Creating Users Model

---
+++ models/users.js
@@ -0,0 +1,36 @@
+
+const {Schema, model} = require('mongoose')
+
+const UsersSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'It needed the name']
+    },
+    email: {
+        type: String,
+        required: [true, 'It needed the email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'It needed the password']
+    },
+    img: {
+        type: String
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = model('User', UsersSchema)
