c2a0297a7c19836c4b61c4a0714c7206f807d76e
yankie <yakielarapacheco@yahoo.com>
2021-12-12 22:22:53 -0500

    Refactoring for using the helper method to validate the user role as custom middleware

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,12 @@
 const {Router} = require('express')
 const {check} = require("express-validator");
 
-const Role = require('../models/role')
-
 const {GetUsers,
     PutUsers,
     PostUsers,
     DeleteUsers} = require("../controllers/users");
 const {validateFields} = require("../middlewares/validateFields");
+const {isRoleValid} = require("../helpers/dbValidator");
 
 const router = Router()
 
@@ -20,13 +19,7 @@ router.post('/', [
     check('name', 'Name required').not().isEmpty(),
     check('password', 'Password required and longer than 6 characters').isLength({min: 6}),
     check('email', 'Invalid email').isEmail(),
-    // check('role', 'Invalid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
-    check('role').custom(async (role = 'USER_ROLE') => {
-        const existRole = await Role.findOne({role})
-        if (!existRole) {
-            throw new Error(`Role ${role} is not recognized`)
-        }
-    }),
+    check('role').custom(isRoleValid),
     validateFields
 ], PostUsers)
 
