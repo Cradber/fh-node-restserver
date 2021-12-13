88eca7a087af7482507503b64d197d83853f1925
yankie <yakielarapacheco@yahoo.com>
2021-12-12 21:39:12 -0500

    Checking the correctness of POST parameters

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 
 const {Router} = require('express')
+const {check} = require("express-validator");
+
 const {GetUsers,
     PutUsers,
     PostUsers,
     DeleteUsers} = require("../controllers/users");
+const {validateFields} = require("../middlewares/validateFields");
 
 const router = Router()
 
@@ -11,7 +14,13 @@ router.get('/', GetUsers)
 
 router.put('/:id', PutUsers)
 
-router.post('/', PostUsers)
+router.post('/', [
+    check('name', 'Name required').not().isEmpty(),
+    check('password', 'Password required and longer than 6 characters').isLength({min: 6}),
+    check('email', 'Invalid email').isEmail(),
+    check('role', 'Invalid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validateFields
+], PostUsers)
 
 router.delete('/', DeleteUsers)
 
