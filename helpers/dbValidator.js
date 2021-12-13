48209bf68bf1c16027dec247f6b6cbd118d78351
yankie <yakielarapacheco@yahoo.com>
2021-12-12 22:19:09 -0500

    Adding function helper for check if exist the user email

--- helpers/dbValidator.js
+++ helpers/dbValidator.js
@@ -1,4 +1,5 @@
 const Role = require("../models/role");
+const User = require("../models/users");
 
 const isRoleValid = async (role = 'USER_ROLE') => {
     const existRole = await Role.findOne({role})
@@ -7,7 +8,15 @@ const isRoleValid = async (role = 'USER_ROLE') => {
     }
 }
 
+const isEmailExist = async (email = '') => {
+    const emailExist = await User.findOne({email})
+    if (emailExist) {
+        throw new Error(`Email ${email} is registered`)
+    }
+}
+
 
 module.exports = {
-    isRoleValid
+    isRoleValid,
+    isEmailExist
 }
