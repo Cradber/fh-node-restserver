03c4bf47f5746db721a6150747878ffd8aed8e61
yankie <yakielarapacheco@yahoo.com>
2021-12-12 21:57:25 -0500

    Using custom middleware for verify the user role from the database

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@
 const {Router} = require('express')
 const {check} = require("express-validator");
 
+const Role = require('../models/role')
+
 const {GetUsers,
     PutUsers,
     PostUsers,
@@ -18,7 +20,13 @@ router.post('/', [
     check('name', 'Name required').not().isEmpty(),
     check('password', 'Password required and longer than 6 characters').isLength({min: 6}),
     check('email', 'Invalid email').isEmail(),
-    check('role', 'Invalid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('role', 'Invalid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom(async (role = 'USER_ROLE') => {
+        const existRole = await Role.findOne({role})
+        if (!existRole) {
+            throw new Error(`Role ${role} is not recognized`)
+        }
+    }),
     validateFields
 ], PostUsers)
 
